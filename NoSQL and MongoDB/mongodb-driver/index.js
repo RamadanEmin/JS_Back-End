1458bdc8caf80f1d52fe91ef320be72a811911c8
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-09-05 19:15:56 +0300

    Added 'mongodb-driver' task from NoSQL and MongoDB

---
+++ NoSQL and MongoDB/mongodb-driver/index.js
@@ -0,0 +1,28 @@
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'buyitnow';
+
+async function main() {
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const collection = db.collection('products');
+
+    const findResult = await collection.find({}).toArray();
+    console.log(findResult);
+
+    return 'done.';
+}
+
+main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
