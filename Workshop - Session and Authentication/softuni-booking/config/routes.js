0a5767d6d11666c93e9d73f223ff6d4e3452967c
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-09-16 19:48:17 +0300

    Added User model and home page

--- Workshop - Session and Authentication/softuni-booking/config/routes.js
+++ Workshop - Session and Authentication/softuni-booking/config/routes.js
@@ -1,3 +1,8 @@
+const homeController = require('../controllers/homeController');
+const defaultController = require('../controllers/defaultController');
+
 module.exports = (app) => {
+    app.use(homeController);
    
+    app.all('*', defaultController);
 }
