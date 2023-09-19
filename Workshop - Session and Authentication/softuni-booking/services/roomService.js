d7242d6368d959684928db8fda50fc783d93dbfb
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-09-19 17:09:24 +0300

    Added create controller, action and page

--- Workshop - Session and Authentication/softuni-booking/services/roomService.js
+++ Workshop - Session and Authentication/softuni-booking/services/roomService.js
@@ -4,6 +4,28 @@ function getAll(search, city, fromPrice, toPrice) {
     return Room.find({}).lean();
 }
 
+async function create(roomData, ownerId) {
+    const room = {
+        name: roomData.name,
+        description: roomData.description,
+        city: roomData.city,
+        beds: Number(roomData.beds),
+        price: Number(roomData.price),
+        imgURL: roomData.imgURL,
+        owner: ownerId
+    }
+
+    const missing = Object.entries(room).filter(([k, v]) => !v);
+    if (missing.length > 0) {
+        throw new Error(missing.map(m => `${m[0]} is required`).join('\n'));
+    }
+
+    const result = await Room.create(room);
+
+    return result;
+}
+
 module.exports = {
     getAll,
+    create
 };
