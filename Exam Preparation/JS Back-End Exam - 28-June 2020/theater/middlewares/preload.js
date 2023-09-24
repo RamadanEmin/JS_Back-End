0d0e6d23cef816fe75829927cb5ea5c37ee89918
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-09-24 09:47:37 +0300

    Initial project

---
+++ Exam Preparation/JS Back-End Exam - 28-June 2020/theater/middlewares/preload.js
@@ -0,0 +1,11 @@
+const { getById, getByIdRaw } = require('../services/playService');
+
+module.exports = (lean) => async (req, res, next) => {
+  if (lean) {
+    res.locals.play = await getById(req.params.id);
+  } else {
+    res.locals.play = await getByIdRaw(req.params.id);
+  }
+
+  next();
+};
