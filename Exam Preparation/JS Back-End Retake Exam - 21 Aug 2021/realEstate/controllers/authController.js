de0636d5878dd3e9c7d325cee60d7d53b9f7e519
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-10-04 00:02:53 +0300

    Added Register controller, action and view

---
+++ Exam Preparation/JS Back-End Retake Exam - 21 Aug 2021/realEstate/controllers/authController.js
@@ -0,0 +1,44 @@
+const authController = require("express").Router();
+
+const { isGuest } = require("../middlewares/guards");
+const { register } = require("../services/userService");
+const { parseError } = require("../util/parser");
+
+
+authController.get("/register", isGuest(), (req, res) => {
+  res.render("register", { title: "Register Page" });
+});
+
+authController.post("/register", isGuest(), async (req, res) => {
+  try {
+    if (req.body.fullName.split(' ').length !== 2) {
+      throw new Error('Fullname is invalid');
+    }
+    if (req.body.username.length < 5) {
+      throw new Error('Username should be at least 5 characters long');
+    }
+    if (req.body.password.length < 5) {
+      throw new Error('Password should be at least 4 characters long');
+    }
+    if (req.body.password !== req.body.repass) {
+      throw new Error('Passwords don\'t match');
+    }
+    const token = await register(req.body.fullName,req.body.username, req.body.password);
+
+    res.cookie("token", token);
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    const errors = parseError(error);
+    res.render("register", {
+      title: "Register Page",
+      errors,
+      body: {
+        username: req.body.username,
+        fullName: req.body.fullName
+      },
+    });
+  }
+});
+
+module.exports = authController;
