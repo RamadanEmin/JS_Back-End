9b5420674edc762621c2b246bbb1deca8b7e0871
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-10-03 16:56:20 +0300

    Added Create Trip controller, action and view

--- Exam Preparation/JS Back-End Exam - 27 June 2021/sharedtrip/config/routes.js
+++ Exam Preparation/JS Back-End Exam - 27 June 2021/sharedtrip/config/routes.js
@@ -1,8 +1,10 @@
 const authController = require('../controllers/auth');
+const tripController = require('../controllers/trip');
 const homeController = require('../controllers/home');
 
 module.exports = (app) => {
     app.use(authController);
+    app.use(tripController);
     app.use(homeController);
 
     app.get('*', (req, res) => {
--- Exam Preparation/JS Back-End Exam - 27 June 2021/sharedtrip/services/trip.js
+++ Exam Preparation/JS Back-End Exam - 27 June 2021/sharedtrip/services/trip.js
@@ -4,6 +4,12 @@ async function getAllTrips() {
     return Trip.find({}).lean();
 }
 
+async function createTrip(trip) {
+    const result = new Trip(trip);
+    await result.save();
+}
+
 module.exports = {
     getAllTrips,
+    createTrip,
 };
