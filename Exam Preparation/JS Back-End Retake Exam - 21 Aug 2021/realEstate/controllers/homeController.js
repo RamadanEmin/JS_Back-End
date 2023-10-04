decd9ca84e1f0b92c42c1f25336963a467cf87fa
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-10-04 16:33:08 +0300

    Added Catalog controller, action and view

--- Exam Preparation/JS Back-End Retake Exam - 21 Aug 2021/realEstate/controllers/homeController.js
+++ Exam Preparation/JS Back-End Retake Exam - 21 Aug 2021/realEstate/controllers/homeController.js
@@ -1,11 +1,17 @@
 const homeController = require("express").Router();
 
-const { getRecent } = require("../services/housingService");
+const { getRecent, getAll } = require("../services/housingService");
 
 homeController.get("/", async (req, res) => {
     const housings = await getRecent();
 
     res.render("home", { title: "Home Page", housings });
+});
+
+homeController.get("/housings", async (req, res) => {
+    const housings = await getAll();
+
+    res.render("housings", { title: "Housings For Rent", housings });
 });
 
 module.exports = homeController;
--- Exam Preparation/JS Back-End Retake Exam - 21 Aug 2021/realEstate/services/housingService.js
+++ Exam Preparation/JS Back-End Retake Exam - 21 Aug 2021/realEstate/services/housingService.js
@@ -4,6 +4,17 @@ async function getRecent() {
     return Housing.find({}).sort({ createdAt: -1 }).limit(3).lean();
 }
 
+async function getAll(search) {
+    const query = {};
+
+    if (search) {
+        query.type = new RegExp(search, 'i');
+    }
+
+    return Housing.find(query).lean();
+}
+
 module.exports = {
     getRecent,
+    getAll,
 };
