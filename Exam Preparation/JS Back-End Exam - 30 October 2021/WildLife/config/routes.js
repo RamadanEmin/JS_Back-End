4b95776d293b405f30dc65f3ae1d268cbe7f8908
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-10-05 16:00:21 +0300

    Added Home controller, action and view

--- Exam Preparation/JS Back-End Exam - 30 October 2021/WildLife/config/routes.js
+++ Exam Preparation/JS Back-End Exam - 30 October 2021/WildLife/config/routes.js
@@ -1,3 +1,8 @@
+const homeController = require('../controllers/home');
+
 module.exports = (app) => {
-  
+    app.use(homeController);
+    app.use((req, res, next) => {
+        res.render('404', { title: 'Error Page' });
+    });
 }
