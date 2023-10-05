99279f3023698c0bf4c5ac4baff149ce8315e201
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-10-05 21:02:49 +0300

    Added Edit post controller, action and view

--- Exam Preparation/JS Back-End Exam - 30 October 2021/WildLife/controllers/post.js
+++ Exam Preparation/JS Back-End Exam - 30 October 2021/WildLife/controllers/post.js
@@ -1,6 +1,6 @@
 const { isUser } = require('../middleware/guards');
-const { createPost } = require('../services/post');
-const { mapErrors } = require('../util/mappers');
+const { createPost, getPostById, updatePost } = require('../services/post');
+const { mapErrors, postViewModel } = require('../util/mappers');
 
 
 const router = require('express').Router();
@@ -28,6 +28,45 @@ router.post('/create', isUser(), async (req, res) => {
         console.error(err);
         const errors = mapErrors(err);
         res.render('create', { title: 'Create Post', errors, data: post });
+    }
+});
+
+router.get('/edit/:id', isUser(), async (req, res) => {
+    const id = req.params.id;
+    const post = postViewModel(await getPostById(id));
+
+    if (req.session.user._id != post.author._id) {
+        return res.redirect('/login');
+    }
+
+    res.render('edit', { title: 'Edit Post', post })
+});
+
+router.post('/edit/:id', isUser(), async (req, res) => {
+    const id = req.params.id;
+    const existing = postViewModel(await getPostById(id));
+
+    if (req.session.user._id != existing.author._id) {
+        return res.redirect('/login');
+    }
+
+    const post = {
+        title: req.body.title,
+        keyword: req.body.keyword,
+        location: req.body.location,
+        date: req.body.date,
+        image: req.body.image,
+        description: req.body.description
+    };
+
+    try {
+        await updatePost(id, post);
+        res.redirect('/catalog/' + id);
+    } catch (err) {
+        console.error(err);
+        const errors = mapErrors(err);
+        post._id = id;
+        res.render('edit', { title: 'Edit Post', errors, post })
     }
 });
 
