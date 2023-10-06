ef3310be2441f8f9d286d666999d6cb42ae923b5
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-10-06 18:02:43 +0300

    Added Login / Logout controller, action and view

--- Exam Preparation/JS Back-End Retake Exam - 14 Dec 2021/artGallery/controllers/authController.js
+++ Exam Preparation/JS Back-End Retake Exam - 14 Dec 2021/artGallery/controllers/authController.js
@@ -1,8 +1,8 @@
 const authController = require('express').Router();
 const { body, validationResult } = require('express-validator');
 
-const { isGuest } = require('../middlewares/guards');
-const { register } = require('../services/userService');
+const { isGuest, hasUser } = require('../middlewares/guards');
+const { register, login } = require('../services/userService');
 const { parseError } = require('../util/parser');
 
 
@@ -39,5 +39,31 @@ authController.post('/register', isGuest(),
             });
         }
     });
+
+authController.get('/login', isGuest(), (req, res) => {
+    res.render('login', { title: 'Login Page' });
+});
+
+authController.post('/login', isGuest(), async (req, res) => {
+    try {
+        const token = await login(req.body.username, req.body.password);
+        res.cookie('token', token);
+        res.redirect('/');
+    } catch (error) {
+        const errors = parseError(error);
+        res.render('login', {
+            title: 'Login Page',
+            errors,
+            body: {
+                username: req.body.username,
+            },
+        });
+    }
+});
+
+authController.get('/logout',hasUser(), (req, res) => {
+    res.clearCookie('token');
+    res.redirect('/');
+});
 
 module.exports = authController;
