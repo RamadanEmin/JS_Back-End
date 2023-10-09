471a189269029475895318531d2feab2ba7baac6
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-10-09 16:11:33 +0300

    Added Login / Logout controller, action and view

--- Exam Preparation/JS Back-End Exam - 19 April 2022/auctionHouse/controllers/authController.js
+++ Exam Preparation/JS Back-End Exam - 19 April 2022/auctionHouse/controllers/authController.js
@@ -1,5 +1,5 @@
-const { isGuest } = require('../middlewares/guards');
-const { register } = require('../services/userService');
+const { isGuest, hasUser } = require('../middlewares/guards');
+const { register, login } = require('../services/userService');
 const { parseError } = require('../util/parser');
 
 const authController = require('express').Router();
@@ -45,6 +45,34 @@ authController.post('/register', isGuest(), async (req, res) => {
             },
         });
     }
+});
+
+authController.get('/login', isGuest(), (req, res) => {
+    res.render('login', {
+        title: 'Login Page',
+    });
+});
+
+authController.post('/login', isGuest(), async (req, res) => {
+    try {
+        const token = await login(req.body.email, req.body.password);
+        res.cookie('token', token);
+        res.redirect('/');
+    } catch (error) {
+        const errors = parseError(error);
+        res.render('login', {
+            title: 'Login Page',
+            errors,
+            body: {
+                email: req.body.email
+            },
+        });
+    }
+});
+
+authController.get('/logout', hasUser(), (req, res) => {
+    res.clearCookie('token');
+    res.redirect('/');
 });
 
 module.exports = authController;
