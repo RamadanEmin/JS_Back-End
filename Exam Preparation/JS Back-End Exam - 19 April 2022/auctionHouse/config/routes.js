4d879f96274f74b558064a0edf536c082b0ad8a2
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-10-09 13:56:20 +0300

    Added Home controller, action and view

--- Exam Preparation/JS Back-End Exam - 19 April 2022/auctionHouse/config/routes.js
+++ Exam Preparation/JS Back-End Exam - 19 April 2022/auctionHouse/config/routes.js
@@ -1,4 +1,8 @@
+const homeController = require("../controllers/homeController");
 
 module.exports = (app) => {
-
+  app.use("/", homeController);
+  app.use((req, res, nex) => {
+    res.render('404', { title: 'Error Page' });
+  });
 };
