2413851ef79e77f5c39560bf87fe8ffdbb448258
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-10-10 20:01:32 +0300

    Added Catalog controller, action and view

--- Exam Preparation/JS Back-End Exam - 19 April 2022/auctionHouse/controllers/homeController.js
+++ Exam Preparation/JS Back-End Exam - 19 April 2022/auctionHouse/controllers/homeController.js
@@ -1,7 +1,13 @@
+const { getAll } = require('../services/auctionService');
+
 const homeController = require('express').Router();
 
 homeController.get('/', (req, res) => {
     res.render('home', { title: 'Home Page' });
+});
+
+homeController.get('/catalog', async (req, res) => {
+    res.render('catalog', { title: 'Catalog', auctions: await getAll() });
 });
 
 module.exports = homeController;
--- Exam Preparation/JS Back-End Exam - 19 April 2022/auctionHouse/services/auctionService.js
+++ Exam Preparation/JS Back-End Exam - 19 April 2022/auctionHouse/services/auctionService.js
@@ -1,7 +1,10 @@
 const Auction = require('../models/Auction');
 
+const getAll = async () => Auction.find({}).lean();
+
 const create = async (data) => Auction.create(data);
 
 module.exports = {
+    getAll,
     create,
 };
