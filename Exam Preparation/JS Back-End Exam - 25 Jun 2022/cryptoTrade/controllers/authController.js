61096a4d5c79d88005d4925a0f6dea0c18bc31fa
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-10-11 19:09:12 +0300

    Added Register controller, action and view

---
+++ Exam Preparation/JS Back-End Exam - 25 Jun 2022/cryptoTrade/controllers/authController.js
@@ -0,0 +1,43 @@
+const authController = require('express').Router();
+
+const { isGuest } = require('../middlewares/guards');
+const { register } = require('../services/userService');
+const { parseError } = require('../util/parser');
+
+authController.get('/register',isGuest(), (req, res) => {
+    res.render('register', { title: 'Register Page' });
+});
+
+authController.post('/register',isGuest(), async (req, res) => {
+    try {
+        if (req.body.username.length < 5) {
+            throw new Error('Username should be at least 5 characters');
+        }
+        if (req.body.email.length < 10) {
+            throw new Error('Email should be at least 10 characters');
+        }
+        if (req.body.password.length < 4) {
+            throw new Error('Password should be at least 4 characters');
+        }
+        if (req.body.password != req.body.repass) {
+            throw new Error('Password don\'t match');
+        }
+        const token = await register(req.body.email, req.body.username, req.body.password);
+
+        res.cookie('token', token);
+        res.redirect('/');
+    } catch (error) {
+        console.log(error);
+        const errors = parseError(error);
+        res.render('register', {
+            title: 'Register Page',
+            errors,
+            body: {
+                email: req.body.email,
+                username: req.body.username
+            },
+        });
+    }
+});
+
+module.exports = authController;
