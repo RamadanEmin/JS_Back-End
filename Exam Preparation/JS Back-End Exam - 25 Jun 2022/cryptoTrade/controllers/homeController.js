542fcd3a0cfb36750baff8d95d0824bbf22a757e
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-10-11 21:29:44 +0300

    Added Catalog controller, action and view

--- Exam Preparation/JS Back-End Exam - 25 Jun 2022/cryptoTrade/controllers/homeController.js
+++ Exam Preparation/JS Back-End Exam - 25 Jun 2022/cryptoTrade/controllers/homeController.js
@@ -1,7 +1,15 @@
+const { getAll } = require('../services/cryptoService');
+
 const homeController = require('express').Router();
 
 homeController.get('/', (req, res) => {
     res.render('home', { title: 'Home' });
+});
+
+homeController.get('/catalog', async (req, res) => {
+    const crypto = await getAll();
+
+    res.render('catalog', { title: 'Catalog', crypto });
 });
 
 module.exports = homeController;
--- Exam Preparation/JS Back-End Exam - 25 Jun 2022/cryptoTrade/services/cryptoService.js
+++ Exam Preparation/JS Back-End Exam - 25 Jun 2022/cryptoTrade/services/cryptoService.js
@@ -1,7 +1,10 @@
 const Crypto = require('../models/Crypto');
 
+const getAll = async () =>  Crypto.find({}).lean();
+
 const create = async (data) => Crypto.create(data);
 
 module.exports = {
+    getAll,
     create,
 };
