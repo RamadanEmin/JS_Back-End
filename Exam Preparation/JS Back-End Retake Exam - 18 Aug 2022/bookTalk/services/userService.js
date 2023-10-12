9811653e5d92011b68770f4787da93372df31fba
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-10-12 21:29:31 +0300

    Added Register controller, action and view

---
+++ Exam Preparation/JS Back-End Retake Exam - 18 Aug 2022/bookTalk/services/userService.js
@@ -0,0 +1,47 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const JWT_SECRET = "q390vnodzmgszdfgsdz";
+
+async function register(email, username, password) {
+    const existing = await User.findOne({ email }).collation({
+        locale: "en",
+        strength: 2,
+    });
+    if (existing) {
+        throw new Error("Email is taken!");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+        email,
+        username,
+        hashedPassword,
+    });
+
+    const token = createSession(user);
+
+    return token;
+}
+
+function createSession({ _id, email, username }) {
+    const payload = {
+        _id,
+        email,
+        username
+    };
+
+    const token = jwt.sign(payload, JWT_SECRET);
+    return token;
+}
+
+function verifyToken(token) {
+    return jwt.verify(token, JWT_SECRET);
+}
+
+module.exports = {
+    register,
+    verifyToken,
+};
