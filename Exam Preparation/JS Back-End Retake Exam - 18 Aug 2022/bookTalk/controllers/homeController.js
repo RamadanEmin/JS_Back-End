d879bf039c4e0648e4c9c6b2fc2eb487b041320a
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-10-13 20:58:55 +0300

    Added Book catalog controller, action and view

--- Exam Preparation/JS Back-End Retake Exam - 18 Aug 2022/bookTalk/controllers/homeController.js
+++ Exam Preparation/JS Back-End Retake Exam - 18 Aug 2022/bookTalk/controllers/homeController.js
@@ -1,7 +1,15 @@
+const { getAll } = require('../services/bookService');
+
 const homeController = require('express').Router();
 
 homeController.get('/', (req, res) => {
   res.render('home', { title: 'Home Page' });
+});
+
+homeController.get('/catalog', async (req, res) => {
+  const books = await getAll();
+
+  res.render('catalog', { title: 'Catalog Page', books });
 });
 
 module.exports = homeController;
--- Exam Preparation/JS Back-End Retake Exam - 18 Aug 2022/bookTalk/services/bookService.js
+++ Exam Preparation/JS Back-End Retake Exam - 18 Aug 2022/bookTalk/services/bookService.js
@@ -1,7 +1,10 @@
 const Book = require('../models/Book');
 
+const getAll = async () => Book.find({}).lean();
+
 const create = async (data) => Book.create(data);
 
 module.exports = {
+    getAll,
     create,
 };
