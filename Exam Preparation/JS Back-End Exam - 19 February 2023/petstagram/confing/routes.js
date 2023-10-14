922e5d85f9a909c3a6c194c3530c9dcde83f0302
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-10-14 14:26:12 +0300

    Added Home, Error page controller and view

--- Exam Preparation/JS Back-End Exam - 19 February 2023/petstagram/confing/routes.js
+++ Exam Preparation/JS Back-End Exam - 19 February 2023/petstagram/confing/routes.js
@@ -1,3 +1,10 @@
+const homeController = require('../controllers/homeController');
+
 module.exports = (app) => {
- 
+  app.use('/', homeController);
+  app.use((req, res, next) => {
+    res.render('404', {
+      title: 'Error Page'
+    });
+  });
 };
