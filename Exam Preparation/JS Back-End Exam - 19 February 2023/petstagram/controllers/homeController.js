383e70e4d7bacc25b4081a9136a250c6f8a7b4f1
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-10-15 09:34:13 +0300

    Added Photo catalog controller, action and view

--- Exam Preparation/JS Back-End Exam - 19 February 2023/petstagram/controllers/homeController.js
+++ Exam Preparation/JS Back-End Exam - 19 February 2023/petstagram/controllers/homeController.js
@@ -1,8 +1,19 @@
+const { getAll } = require('../services/photoService');
+
 const homeController = require('express').Router();
 
 homeController.get('/', (req, res) => {
   res.render('home', {
     title: 'Home'
+  });
+});
+
+homeController.get('/catalog', async (req, res) => {
+  const photos = await getAll();
+
+  res.render('catalog', {
+    title: 'Catalog',
+    photos
   });
 });
 
--- Exam Preparation/JS Back-End Exam - 19 February 2023/petstagram/services/photoService.js
+++ Exam Preparation/JS Back-End Exam - 19 February 2023/petstagram/services/photoService.js
@@ -1,7 +1,10 @@
 const Photo = require("../models/Photo");
 
+const getAll = async () => Photo.find({}).populate('owner').lean();
+
 const create = async (data) => Photo.create(data);
 
 module.exports = {
+  getAll,
   create,
 };
