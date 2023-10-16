6804eef5800c01c353acb7497d201446fb2cae35
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-10-16 22:11:45 +0300

    Added Game catalog controller, action and view

--- Exam Preparation/JS Back-End Retake Exam - 13 Apr 2023/gamingTeam/controllers/homeController.js
+++ Exam Preparation/JS Back-End Retake Exam - 13 Apr 2023/gamingTeam/controllers/homeController.js
@@ -1,7 +1,15 @@
+const { getAll } = require('../services/gameService');
+
 const homeController = require('express').Router();
 
 homeController.get('/', (req, res) => {
     res.render('home', { title: 'Home Page', idStyle: 'home' });
+});
+
+homeController.get('/catalog', async (req, res) => {
+    const games = await getAll();
+
+    res.render('catalog', { title: 'Catalog Page', idStyle: 'catalog', games });
 });
 
 module.exports = homeController;
--- Exam Preparation/JS Back-End Retake Exam - 13 Apr 2023/gamingTeam/services/gameService.js
+++ Exam Preparation/JS Back-End Retake Exam - 13 Apr 2023/gamingTeam/services/gameService.js
@@ -1,7 +1,10 @@
 const Game = require('../models/Game');
 
+const getAll = async () => Game.find({}).lean();
+
 const create = async (data) => Game.create(data);
 
 module.exports = {
+    getAll,
     create,
 };
