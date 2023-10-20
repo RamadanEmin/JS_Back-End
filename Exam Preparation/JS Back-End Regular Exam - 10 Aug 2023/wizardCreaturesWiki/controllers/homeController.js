a800c022a239cc80211e042d2bcd64e6abbf2403
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-10-20 21:14:20 +0300

    Added Catalog controller, action and view

--- Exam Preparation/JS Back-End Regular Exam - 10 Aug 2023/wizardCreaturesWiki/controllers/homeController.js
+++ Exam Preparation/JS Back-End Regular Exam - 10 Aug 2023/wizardCreaturesWiki/controllers/homeController.js
@@ -1,7 +1,15 @@
 const homeController = require('express').Router();
 
+const { getAll } = require('../services/creatureService');
+
 homeController.get('/', (req, res) => {
     res.render('home', { title: 'Home Page' });
+});
+
+homeController.get('/catalog', async (req, res) => {
+    const creatures = await getAll();
+
+    res.render('catalog', { title: 'Catalog Page', creatures });
 });
 
 module.exports = homeController;
--- Exam Preparation/JS Back-End Regular Exam - 10 Aug 2023/wizardCreaturesWiki/services/creatureService.js
+++ Exam Preparation/JS Back-End Regular Exam - 10 Aug 2023/wizardCreaturesWiki/services/creatureService.js
@@ -1,7 +1,10 @@
 const Creature = require('../models/Creature');
 
+const getAll = async () => Creature.find({}).lean();
+
 const create = async (data) => Creature.create(data);
 
 module.exports = {
+    getAll,
     create,
 };
