24c216c14ec36268b3aba7bdf004be7ac0ad5fdb
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-06-22 18:56:35 +0300

    Added Login and Register page

--- Exam Preparation/JS Back-End Regular Exam - 21 Oct 2023/secondHandElectronics/src/config/configRoutes.js
+++ Exam Preparation/JS Back-End Regular Exam - 21 Oct 2023/secondHandElectronics/src/config/configRoutes.js
@@ -1,7 +1,11 @@
 const { homeController } = require('../controllers/homeController');
+const { userController } = require('../controllers/userController');
+const { electronicsController } = require('../controllers/electronicsController');
 
 function configRoutes(app) {
     app.use(homeController);
+    app.use(userController);
+    app.use(electronicsController);
     app.get('*', (req, res) => {
         res.render('404');
     });
---
+++ Exam Preparation/JS Back-End Regular Exam - 21 Oct 2023/secondHandElectronics/src/controllers/userController.js
@@ -0,0 +1,73 @@
+const { Router } = require('express');
+const { login, register } = require('../services/user');
+const { isGuest } = require('../middlewares/guards');
+const { body, validationResult } = require('express-validator')
+const { createToken } = require('../services/jwt');
+const { parseError } = require('../util');
+
+const userController = Router();
+
+userController.get('/register', isGuest(), async (req, res) => {
+
+    res.render('register', { title: 'Register Page' });
+});
+
+userController.post('/register', isGuest(),
+    body('username').trim().isLength({ min: 3 }).withMessage('Username must be at least 3 character long'),
+    body('email').trim().isEmail().isLength({ min: 10 }).withMessage('Email must be at least 10 character long'),
+    body('password').trim().isLength({ min: 4 }).withMessage('Password must be at least 4 characters long'),
+    body('repass').trim().custom((value, { req }) => value == req.body.password).withMessage('Passwords don\'t match'),
+    async (req, res) => {
+        const { email, password, username } = req.body;
+
+        try {
+            const validation = validationResult(req);
+
+            if (validation.errors.length) {
+                throw validation.errors;
+            }
+
+            const result = await register(email, password, username);
+            const token = createToken(result);
+
+            res.cookie('token', token);
+
+            res.redirect('/');
+        } catch (err) {
+            console.log(err);
+            res.render('register', { data: { email, username }, errors: parseError(err).errors });
+        }
+    }
+)
+
+userController.get('/logout', (req, res) => {
+    res.clearCookie('token');
+    res.redirect('/');
+});
+
+userController.get('/login', isGuest(), async (req, res) => {
+    res.render('login', { title: 'Login Page' });
+});
+
+userController.post('/login', isGuest(),
+    body('email').trim(),
+    body('password').trim(),
+    async (req, res) => {
+        const { email, password } = req.body;
+
+        try {
+            const result = await login(email, password);
+            const token = createToken(result);
+
+            res.cookie('token', token);
+
+            res.redirect('/');
+        } catch (err) {
+            res.render('login', { data: { email }, errors: parseError(err).errors });
+        }
+    }
+)
+
+module.exports = {
+    userController
+};
