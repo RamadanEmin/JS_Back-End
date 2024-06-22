56ed0a216fd2bc8d417cf5ce54e3e2b3a517d182
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-06-22 20:01:03 +0300

    Added Edit page and delete functionality

--- Exam Preparation/JS Back-End Regular Exam - 21 Oct 2023/secondHandElectronics/src/controllers/electronicsController.js
+++ Exam Preparation/JS Back-End Regular Exam - 21 Oct 2023/secondHandElectronics/src/controllers/electronicsController.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { create } = require('../services/electronics');
+const { create, getById, update, deleteById } = require('../services/electronics');
 const { body, validationResult } = require('express-validator');
 const { isUser } = require('../middlewares/guards');
 const { parseError } = require('../util');
@@ -35,6 +35,63 @@ electronicsController.post('/create', isUser(),
         }
     }
 );
+
+electronicsController.get('/edit/:id', isUser(), async (req, res) => {
+    const fetchData = await getById(req.params.id);
+
+    if (!fetchData) {
+        res.render('404');
+    }
+
+    const isOwner = req.user?._id == fetchData.owner.toString();
+
+    if (!isOwner) {
+        res.redirect('/login');
+        return;
+    }
+
+    res.render('edit', { title: 'Edit Page', data: fetchData });
+})
+
+electronicsController.post('/edit/:id', isUser(),
+    body('name').trim().isLength({ min: 10 }).withMessage('Name should be at least 10 characters long'),
+    body('type').trim().isLength({ min: 2 }).withMessage('Type must be at least 2 characters long'),
+    body('damages').trim().isLength({ min: 0 }).withMessage('Damages should be at least 10 characters long'),
+    body('image').trim().isURL({ require_tld: false }).withMessage('Electronic\'s image must be a valid URL'),
+    body('description').trim().isLength({ min: 10, max: 200 }).withMessage('Description should be between 10 and 200'),
+    body('production').trim().isInt({ min: 1900, max: 2023 }).withMessage('Production should be between 1900 and 2023'),
+    body('exploatation').trim().isInt({ min: 0 }).withMessage('Exploatation must to be a positive number'),
+    body('price').trim().isInt({ min: 0 }).withMessage('Price must to be a positive number'),
+    async (req, res) => {
+        const electronicId = req.params.id;
+        const userId = req.user._id;
+
+        try {
+            const validation = validationResult(req);
+
+            if (validation.errors.length) {
+                throw validation.errors;
+            }
+
+            await update(electronicId, req.body, userId);
+            res.redirect('/catalog/' + electronicId);
+        } catch (err) {
+            res.render('create', { data: req.body, errors: parseError(err).errors });
+        }
+    }
+);
+
+electronicsController.get('/delete/:id', isUser(), async (req, res) => {
+    const electronicId = req.params.id;
+    const userId = req.user._id;
+
+    try {
+        await deleteById(electronicId, userId);
+        res.redirect('/catalog');
+    } catch (err) {
+        res.redirect('/catalog/' + electronicId);
+    }
+});
 
 module.exports = {
     electronicsController
--- Exam Preparation/JS Back-End Regular Exam - 21 Oct 2023/secondHandElectronics/src/services/electronics.js
+++ Exam Preparation/JS Back-End Regular Exam - 21 Oct 2023/secondHandElectronics/src/services/electronics.js
@@ -30,9 +30,50 @@ async function create(data, ownerId) {
   return record;
 }
 
+async function update(electronicId, data, userId) {
+  const record = await Electronic.findById(electronicId);
+
+  if (!record) {
+    throw new ReferenceError('Record not found!' + electronicId);
+  }
+
+  if (record.owner.toString() != userId) {
+    throw new Error('Access Denied!');
+  }
+
+  record.name = data.name;
+  record.type = data.type;
+  record.damages = data.damages;
+  record.image = data.image;
+  record.description = data.description;
+  record.production = data.production;
+  record.exploatation = data.exploatation;
+  record.price = data.price;
+
+  await record.save();
+
+  return record;
+}
+
+async function deleteById(electronicId, userId) {
+  const record = await Electronic.findById(electronicId);
+
+  if (!record) {
+    throw new ReferenceError('Record not found!' + electronicId);
+  }
+
+  if (record.owner.toString() != userId) {
+    throw new Error('Access Denied!');
+  }
+
+  await Electronic.findByIdAndDelete(electronicId);
+}
+
 module.exports = {
   getAll,
   getById,
+  update,
+  deleteById,
   create,
-  getRecent,
+  getRecent
 }
