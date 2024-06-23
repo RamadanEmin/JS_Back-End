b36b594b2efd9a9f9966a5ea0d77c8144007de60
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-06-23 10:38:51 +0300

    Added Catalog page

--- Exam Preparation/JS Back-End Retake Exam - 13 Dec 2023/courseBook/src/controllers/homeController.js
+++ Exam Preparation/JS Back-End Retake Exam - 13 Dec 2023/courseBook/src/controllers/homeController.js
@@ -1,11 +1,16 @@
 const { Router } = require('express');
-const { getRecent } = require('../services/courseService');
+const { getAll, getRecent } = require('../services/courseService');
 
 const homeController = Router();
 
 homeController.get('/', async (req, res) => {
     const courses = await getRecent();
     res.render('home', { title: 'Home Page',courses });
+});
+
+homeController.get('/catalog', async (req, res) => {
+    const courses = await getAll();
+    res.render('catalog', { title: 'Catalog Page', courses });
 });
 
 module.exports = {
--- Exam Preparation/JS Back-End Retake Exam - 13 Dec 2023/courseBook/src/services/courseService.js
+++ Exam Preparation/JS Back-End Retake Exam - 13 Dec 2023/courseBook/src/services/courseService.js
@@ -1,9 +1,14 @@
 const { Course } = require('../models/Course');
 
+async function getAll() {
+  return Course.find().lean();
+}
+
 function getRecent() {
   return Course.find().sort({ _id: -1 }).limit(3).lean();
 }
 
 module.exports = {
-  getRecent
+  getAll,
+  getRecent,
 }
