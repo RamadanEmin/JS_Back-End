feb51f36b62314f746dc5f9d44abb95658aeac91
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-06-23 11:02:22 +0300

    Added Login and Register page

--- Exam Preparation/JS Back-End Retake Exam - 13 Dec 2023/courseBook/src/config/configRoutes.js
+++ Exam Preparation/JS Back-End Retake Exam - 13 Dec 2023/courseBook/src/config/configRoutes.js
@@ -1,7 +1,9 @@
 const { homeController } = require('../controllers/homeController');
+const { userController } = require('../controllers/userController');
 
 function configRoutes(app) {
     app.use(homeController);
+    app.use(userController);
     app.get('*', (req, res) => {
         res.render('404');
     });
